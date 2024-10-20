2c7ea0a34ee5b77ac4229c87e3fff9817b23a27f
spinner99-bit <121241376+spinner99-bit@users.noreply.github.com>
2024-10-20 19:52:32 +0800

    Update providers.js

--- providers.js
+++ providers.js
@@ -88,6 +88,7 @@ document.addEventListener("DOMContentLoaded", function() {
         "Rollex11": "Element/GameLogo/Rollex11.png",
         "918Kiss 2": "Element/GameLogo/918Kiss2.png",
         "918Kiss Html5": "Element/GameLogo/918KissHtml5.png",
+        "Mega H5": "Element/GameLogo/MegaH5.png",
         "V Power": "Element/GameLogo/VPower.png",
         "King855": "Element/GameLogo/King855.png",
         "C8Play": "Element/GameLogo/C8Play.png",
