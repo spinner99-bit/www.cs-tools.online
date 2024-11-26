74104cfdaff31b325cc0135bfd8cef134aadd3a2
spinner99-bit <121241376+spinner99-bit@users.noreply.github.com>
2024-11-27 04:54:10 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzIr50gDiDsWCcrAl7H0oayuWpC-0-oYZ9wSNTVdBhMrg9AnZew_IFZ4jZgNQoLOlcLMA/exec'; // 替换为你的 Google Apps Script URL
+const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzuE_iqBjQIWRmMRF-B6fjgZ1VVjGQVl3QoCwu5zNBfNEhVc83KQ3CXdgpicRrCj0Z4zg/exec'; // 替换为你的 Google Apps Script URL
 
 document.addEventListener('contextmenu', event => event.preventDefault());
 
@@ -48,13 +48,9 @@ async function submitLogin() {
     localStorage.setItem('username', username);
     localStorage.setItem('password', password);
     localStorage.setItem('company', result.fullName);
-    localStorage.setItem('phoneNumber', result.phoneNumber);
-    localStorage.setItem('option1', result.option1);
-    localStorage.setItem('number2', result.number2);
-    localStorage.setItem('option2', result.option2);
 
     // 登录成功后跳转到 index.html
-    window.location.href = 'index.html';
+    window.location.href = 'tips_Game';
   } else {
         // 如果登录失败，显示错误信息
         document.getElementById('loginMessage').textContent = result.message || "Login failed, please try again.";
