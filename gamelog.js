4606184a22031dce16d40e8ae0aec12d15ffbcbb
spinner99-bit <121241376+spinner99-bit@users.noreply.github.com>
2024-12-02 07:00:09 +0800

    Update gamelog.js

--- gamelog.js
+++ gamelog.js
@@ -7,6 +7,17 @@ const tableData = [];
             e.preventDefault();
         }
     });
+
+    document.addEventListener("DOMContentLoaded", () => {
+        // 从 localStorage 获取用户名
+        const username = localStorage.getItem("username");
+    
+        // 检查用户名是否为 admin 或 MB33
+        if (username === "admin" || username === "MB33") {
+            // 显示管理员菜单
+            document.getElementById("menuForAdmin").style.display = "block";
+        }
+    });
     
     // 检查用户登录状态
     function checkLoginStatus() {
